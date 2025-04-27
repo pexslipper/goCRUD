97424fdbeb071149fdd71d4a9b0e32356c50bec8
Vatcharapong <pexslipper@gmail.com>
2025-04-27 16:30:33 +0700

    main func
    add router GET path /ping to test

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 type Users struct {
 	ID    string
 	Fname string
 	Lname string
 	Score float64
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
