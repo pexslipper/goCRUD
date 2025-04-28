418448d817ff523e6d7b58f7e9ce0ff8eb6ae8d2
Vatcharapong <pexslipper@gmail.com>
2025-04-28 23:43:03 +0700

    connect to mysql database
    connect to database test users path to retrieve all users

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	"gorm.io/driver/mysql"
+	_ "gorm.io/driver/mysql"
+)
 
 type Users struct {
-	ID    string
-	Fname string
-	Lname string
-	Score float64
+	gorm.Model
+	Name  string
+	Score uint
 }
 
 func main() {
+
+	dsn := "root@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	// db.AutoMigrate(&Users{})
+
+	// Create sample users
+	// users := []*Users{
+	// 	{Name: "Jinzhu", Score: 18},
+	// 	{Name: "Jackson", Score: 19},
+	// }
+
+	// db.Create(users) // pass a slice to insert multiple row
+
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
+	router.GET("/users", func(c *gin.Context) {
+		var users []Users
+		db.Find(&users)
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"users": users,
 		})
 	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+
+	router.Run("localhost:3000") // listen and serve on 0.0.0.0:8080
 }
